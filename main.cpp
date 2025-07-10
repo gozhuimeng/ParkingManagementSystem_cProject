4977b65c4a9ce906fd93a53526a9bc7d6dac2fd3
meng <gozhuimeng@outlook.com>
2025-07-10 20:32:47 +0800

    09.250710 提交main程序及CMakeLists，完成程序基本内容

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include "core/include/GUI.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    GUI app(argc, argv);
+    app.run();
+
     return 0;
 }
